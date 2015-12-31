8a36871dad0131238d2b0de4f7c24f7cfaa546ca
Noah Nelson <nelson.noah96@gmail.com>
2015-12-31 04:11:11 +0000

    added error handling for writing non-alpha characters

--- morsewrite.swift
+++ morsewrite.swift
@@ -17,12 +17,16 @@ digitalWrite(LED, LOW)
 let words = Process.arguments[1..<Process.arguments.count]
 for word in words {
     for character in word.characters {
-        for symbol in morseTable[character]!.characters {
-            digitalWrite(LED, HIGH)
-            let ticks: UInt32 = (symbol == "." ? 100 : 300)
-            delay(ticks)
-            digitalWrite(LED, LOW)
-            delay(100)
+        if let morseChar = morseTable[character] {
+            for symbol in morseChar.characters {
+                digitalWrite(LED, HIGH)
+                let ticks: UInt32 = (symbol == "." ? 100 : 300)
+                delay(ticks)
+                digitalWrite(LED, LOW)
+                delay(100)
+            }
+        } else {
+            print("Untranslatable character \(character)")
         }
         delay(300)
     }
